a563869414a76c83178a0c7707922f97495c03a1
Malek-ALdesougi <malekaldesougi@gmail.com>
2022-11-22 13:20:17 +0300

    I have fixed the style in the login and register pages with some edits

--- zarqa-E-Mall/routes/web.php
+++ zarqa-E-Mall/routes/web.php
@@ -50,6 +50,10 @@ Route::get('login', function(){
     return view('login');
 });
 
+Route::get('register', function(){
+    return view('register');
+});
+
 
 
 //--------------///////////////////////// DASHBOARD //////////////////////------------//
